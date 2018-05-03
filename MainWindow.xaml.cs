8b587020ba2df8e76d54cbb10c35c194b71c6744
jayfeather1255 <jayfeather1255@yahoo.com.tw>
2018-05-03 11:54:27 +0800

    增加性別選項、美化介面

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,23 +32,30 @@ namespace _0419WpfApp1
             double weight = double.Parse(WeightBox.Text);
             
             double BMI = Math.Round(weight / Math.Pow(height,2),2);
-            result.Text = BMI.ToString();            
+            result.Text = BMI.ToString();
 
             // 根據結果改變顏色
             if (BMI < 18.5)
+            {
                 result.Foreground = Brushes.Red;                
-                         
+            }
 
             else if (18.5 <= BMI & BMI < 24)
-                result.Foreground = Brushes.Green;
+            {
+                result.Foreground = Brushes.Green;               
+            }
 
             else if (BMI > 24)
+            {
                 result.Foreground = Brushes.Goldenrod;                
+            }
+                              
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+            // 將身高數值歸零
+            HeightBar.Value = 0;
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -66,15 +73,32 @@ namespace _0419WpfApp1
 
             // 根據結果改變顏色
             if (BMI < 18.5)
+            {
                 result.Foreground = Brushes.Red;
-
+                resultText.Foreground = Brushes.Red;
+                resultText.Text = "過輕";
+            }
 
             else if (18.5 <= BMI & BMI < 24)
+            {
                 result.Foreground = Brushes.Green;
-
+                resultText.Foreground = Brushes.Green;
+                resultText.Text = "適中";
+            }
+                
             else if (BMI > 24)
+            {
                 result.Foreground = Brushes.Goldenrod;
+                resultText.Foreground = Brushes.Goldenrod;
+                resultText.Text = "過重";
+            }               
 
+        }
+
+        private void ButtonClear2_Click(object sender, RoutedEventArgs e)
+        {
+            // 將體重數值歸零
+            WeightBar.Value = 0;
         }
     }
 }
